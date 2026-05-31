da5a6c64d9fcc84b50a0cfbc7802a0bd4df30c3b
Jakub Skrzypiec <kuba.skrzypiec5@gmail.com>
2026-05-31 11:33:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const demoFormBtn = document.getElementById("demoFormBtn");
 
+if (demoFormBtn) {
+  demoFormBtn.addEventListener("click", () => {
+    alert("To jest formularz pokazowy. W finalnej wersji można podpiąć go pod e-mail lub formularz kontaktowy.");
+  });
+}
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (!header) return;
+
+  if (window.scrollY > 40) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
